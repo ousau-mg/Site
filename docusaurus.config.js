3975a4a1f24622558558f65fd40b69ceef7be054
André Amorim <52294411+Andrelamor@users.noreply.github.com>
2026-03-26 16:28:08 -0300

    Update production URL in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ousau-mg.github.io/Site/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
